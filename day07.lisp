1098f5174d1e21baafdc776936d882c0208b5979
Yuri Karabatov <yuri@yurikarabatov.com>
2021-12-07 11:13:34 +0300

    Day 7 part 2

--- day07.lisp
+++ day07.lisp
@@ -7,13 +7,26 @@
            (vec (read-from-string (concatenate 'string "#(" cleaned ")"))))
       vec)))
 
-(defun fuel-to-index (index crabs)
-  (let ((val (aref crabs index)))
-    (loop for idx from 0 below (length crabs)
-          sum (abs (- val (aref crabs idx))) into total
-          finally (return total))))
+(defun simple-fuel (from to)
+  (abs (- from to)))
+
+(defun fuel-to-index (target crabs fuel-fun)
+  (loop for idx from 0 below (length crabs)
+        sum (funcall fuel-fun target (aref crabs idx)) into total
+        finally (return total)))
 
 (defun day7-1 ()
   (let ((crabs (load-numbers "input.txt")))
     (loop for idx from 0 below (length crabs)
-          minimize (fuel-to-index idx crabs))))
+          minimize (fuel-to-index (aref crabs idx) crabs 'simple-fuel))))
+
+(defun progressive-fuel (from to)
+  (let ((steps (simple-fuel from to)))
+    (floor (* steps (1+ steps)) 2)))
+
+(defun day7-2 ()
+  (let* ((crabs (load-numbers "input.txt"))
+         (pos-min (loop for idx from 0 below (length crabs) minimize (aref crabs idx)))
+         (pos-max (loop for idx from 0 below (length crabs) maximize (aref crabs idx))))
+    (loop for pos from pos-min to pos-max
+          minimize (fuel-to-index pos crabs 'progressive-fuel))))
